eef19e1069ac2b2f416d6ce85227da156e8f79b2
rokkoo <alfonsolasaguirre@gmail.com>
2020-07-12 20:04:56 +0200

    Add metrics

--- metrics/theme.ts
+++ metrics/theme.ts
@@ -1,8 +1,16 @@
-const THEME = {
+import { ThemeProps } from 'CustomTheme';
+
+const THEME: ThemeProps = {
    primaryDark: '#0D0C1D',
    primaryLight: '#EFFFFA',
    primaryHover: '#343078',
    mobile: '576px',
+   desktopMenu: '#B1D4E0',
+   mobileMenu: '#2E8BC0',
+   spacins: {
+      layoutPadding: '0 30%',
+      smallPadding: 8,
+   },
 };
 
 export default THEME;
