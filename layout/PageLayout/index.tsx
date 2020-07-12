456e566721ea904ab837f319597bf4d7e5a34407
rokkoo <alfonsolasaguirre@gmail.com>
2020-07-12 20:06:00 +0200

    Use app menus

--- layout/PageLayout/index.tsx
+++ layout/PageLayout/index.tsx
@@ -3,11 +3,15 @@ import { ThemeProvider } from 'styled-components';
 
 import { THEME } from 'metrics';
 import { GlobalStyle } from 'styles';
+import DesktopMenu from 'components/Menu/Desktop';
+import MobileMenu from 'components/Menu/Mobile';
 
 const PageLayout = ({ children }) => {
    return (
       <ThemeProvider theme={THEME}>
          <GlobalStyle />
+         <DesktopMenu />
+         <MobileMenu />
          {children}
       </ThemeProvider>
    );
